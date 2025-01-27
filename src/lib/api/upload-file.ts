20a687e36884a364ce06a8d8c62109371a75cc20
jpmartel <jp@tractr.net>
2025-01-27 10:54:20 -0500

    updated

--- src/lib/api/upload-file.ts
+++ src/lib/api/upload-file.ts
@@ -7,8 +7,8 @@ export function useUploadFileWithAiSource() {
       const fileExtension = file.name.split('.').pop();
       const uniqueId = crypto.randomUUID();
       const timestamp = Date.now();
-      const filePath = `${uniqueId}/${timestamp}_source_upload_ai_${aiSource}/${file.name}`;
-
+      const filePath = `${uniqueId}/${timestamp}_source_upload_ai_${aiSource}/${timestamp}_${file.name}`;
+      console.log(filePath);
       // we web https://ylgsrkaefxdpomhmocuj.supabase.co/storage/v1/object/files/56d3b862-207f-4027-9d4b-7bb94074fb58/1737735890236_source_upload_ai_false/uh.pdf.pdf
       // mine   https://ylgsrkaefxdpomhmocuj.supabase.co/storage/v1/object/files/254548e6-946e-466e-bc9c-0657221c2594/1737736718925_source_upload_ai_false/uh.pdf.pdf
       const { error } = await supabaseClient.storage.from('files').upload(filePath, file, {
